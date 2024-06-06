8d95307b7e6533a857ca3d939101ceef99da84db
Kondzioplus <111319052+Kondzioplus@users.noreply.github.com>
2024-06-06 19:06:35 +0200

    modyfikacje

--- Lekcje/4/4-2/dist/bundle.js
+++ Lekcje/4/4-2/dist/bundle.js
@@ -115,7 +115,7 @@ module.exports = function stringify(it) {
 "use strict";
 
 
-var core = module.exports = { version: '2.5.3' };
+var core = module.exports = { version: '2.6.12' };
 if (typeof __e == 'number') __e = core; // eslint-disable-line no-undef
 
 /***/ }),
