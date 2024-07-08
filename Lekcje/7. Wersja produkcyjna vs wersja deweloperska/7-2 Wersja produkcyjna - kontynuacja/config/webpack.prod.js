5fe7e1831ae78bc468627a908d3801f44fc7bb84
Kondzioplus <111319052+Kondzioplus@users.noreply.github.com>
2024-07-08 18:03:52 +0200

    Wersja produkcyjna - kontynuacja

---
+++ Lekcje/7. Wersja produkcyjna vs wersja deweloperska/7-2 Wersja produkcyjna - kontynuacja/config/webpack.prod.js
@@ -0,0 +1,43 @@
+const merge = require('webpack-merge');
+const parts = require('./webpack.parts');
+
+const config = {
+    entry: {
+    app: './src/index.js',
+    vendor: ['react', 'react-dom','react-css-modules'],
+  },
+
+  optimization: {
+    splitChunks: {
+      // include all types of chunks
+      chunks: 'all',
+    },
+  },
+
+    resolve: {
+    extensions: ['.*', '.js', '.jsx']
+  },
+    output: {
+    filename: '[name].[chunkhash].js',
+    path: path.resolve(__dirname, './dist'),
+    
+  },
+}
+
+const prod = merge([
+    parts.loadJS(),
+    parts.loadCSS(),
+    parts.loadImages(),
+    parts.loadFonts(),
+    parts.loadHTML({
+      pluginOptions: 
+      {
+        template: path.resolve(__dirname, './../src/index.html'),
+        filename: 'index.html',
+      }
+    }),
+    parts.loadFavicon(),
+    parts.loadSplitChunks(),
+ ]);
+
+module.exports = merge(config, prod);
